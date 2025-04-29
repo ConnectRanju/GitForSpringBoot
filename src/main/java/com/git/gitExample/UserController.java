44151f9f8aff161ef45ee0bea5957157eb5e4aaf
ConnectRanju <sreerag84@github.com>
2025-04-29 12:42:59 -0700

    Commit3:added name in sout.

--- src/main/java/com/git/gitExample/UserController.java
+++ src/main/java/com/git/gitExample/UserController.java
@@ -1,9 +1,11 @@
 package com.git.gitExample;
 
 public class UserController {
+    private String name;
     public void add(){
         //New Method added.
-        System.out.println("Inside Add method");
+        System.out.println("Inside Add method"+name);
+
     }
-    private String name;
+
 }
