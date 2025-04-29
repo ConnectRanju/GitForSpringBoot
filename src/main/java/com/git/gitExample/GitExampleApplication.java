a68e3f8ffb40157063c41aaaae085ced10eed5fe
ConnectRanju <sreerag84@github.com>
2025-04-29 10:32:01 -0700

    comments added.

--- src/main/java/com/git/gitExample/GitExampleApplication.java
+++ src/main/java/com/git/gitExample/GitExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitExampleApplication {
-
+//Main method: Comments added.
 	public static void main(String[] args) {
 		SpringApplication.run(GitExampleApplication.class, args);
 	}
