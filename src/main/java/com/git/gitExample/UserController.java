6b85f690b73c59cc888ffb7634dd91dc5722620b
ConnectRanju <connectranjini@gmail.com>
2025-04-29 12:14:50 -0700

    Update UserController.java

--- src/main/java/com/git/gitExample/UserController.java
+++ src/main/java/com/git/gitExample/UserController.java
@@ -3,6 +3,7 @@ package com.git.gitExample;
 public class UserController {
     public void add(){
         //New Method .
+        System.out.prinln("Inside Add method");
     }
     private String name;
 }
